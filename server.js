5185b5aa0ff35125e3c148d8efc875fb71cced63
gustafstrindevall <gustafstrindevall@hotmail.se>
2018-10-09 10:42:51 +0200

    Setup start script and HEROKU port

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const PORT = process.env.PORT || 5000;
+
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -62,6 +65,6 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(5000, () => {
-    console.log('Server is up on port 5000');
+app.listen(PORT, () => {
+    console.log('Server is up on port: ' + PORT);
 });
